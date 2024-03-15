ccf91169fde373355bcbd415c95d7413bbf053fb
Moon <kwarnkham.moon@gmail.com>
2024-03-15 12:28:43 +0630

    move SoftDeletes to individual model

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -8,7 +8,5 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BaseModel extends Model
 {
-    use SoftDeletes;
-
     protected $guarded = [''];
 }
--- app/Models/Item.php
+++ app/Models/Item.php
@@ -3,8 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Item extends BaseModel
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 }
