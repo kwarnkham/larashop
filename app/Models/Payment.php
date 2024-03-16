428127a9d9ce3ce2bf9e4da9acb4b110ecbd4f18
Moon <kwarnkham.moon@gmail.com>
2024-03-16 12:01:13 +0630

    cast payment type enum

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -2,12 +2,23 @@
 
 namespace App\Models;
 
+use App\Enums\PaymentStatus;
+use App\Enums\PaymentType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
 
 class Payment extends BaseModel
 {
     use HasFactory;
+
+    protected function casts(): array
+    {
+        return [
+            'status' => PaymentStatus::class,
+            'type' => PaymentType::class
+        ];
+    }
 
     public function payable()
     {
