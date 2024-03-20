f4ee611760251b770b9a84d03242ae7a6a75ffcb
Moon <kwarnkham.moon@gmail.com>
2024-03-20 10:02:38 +0630

    authorize channels

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Models\Order;
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+
+Broadcast::channel('App.Models.Order.{id}', function (User $user, $id) {
+    return (int) $user->id === (int) Order::findOrNew($id)->user_id;
+});
+
+Broadcast::channel('App.Models.Order', function ($user) {
+    return $user->hasRole('admin');
 });
